1064e60a4bb297697bd0b9ab265d6f26621249ee
Michael Tang <mzt.tang@gmail.com>
2025-09-18 00:32:32 +1200

    created basic ui and fixed calculator bracket number and rate percentage calculation

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import calculateTax from './utilities/tax-calculator';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [incomeAmount, setIncomeAmount] = useState(0);
+
+  function handleIncomeAmountChange(e) {
+    setIncomeAmount(e.target.value);
+  }
+
+  function handleTaxCalculation(incomeAmount) {
+    return calculateTax(incomeAmount)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>NZ Tax Calculator</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Input your income amount below
         </p>
+        <input id="income-amount" type="number" value={incomeAmount} onChange={handleIncomeAmountChange}/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <h3>
+          Your taxed amount is
+        </h3>
+        </div>
+          {handleTaxCalculation(incomeAmount)}
+        <div>
+      </div>
     </>
   )
 }
--- src/utilities/tax-calculator.js
+++ src/utilities/tax-calculator.js
@@ -1,22 +1,22 @@
 const TAX_BRACKETS = [
   {"bracket" : 0, "rate": 0},
-  {"bracket" : 15000, "rate": 10.5},
+  {"bracket" : 15600, "rate": 10.5},
   {"bracket" : 53500, "rate": 17.5},
   {"bracket" : 78100, "rate": 30},
   {"bracket" : 180000, "rate": 33},
   {"bracket" : Number.MAX_VALUE, "rate": 39}
 ]
 
-function calculateTax(income) {
+export default function calculateTax(income) {
   let taxToPay = 0;
 
   for (let i = 0; i < TAX_BRACKETS.length; i++ ) {
-    if (income >= TAX_BRACKETS[i]) {
+    if (income >= TAX_BRACKETS[i].bracket) {
       taxToPay += calculateMaxBracketAmount(i);
       continue; // move on to the next tax bracket
     } 
     // ( income - last bracket total ) * current bracket rate
-    const maxTaxBracketAmounttoPay = (income - TAX_BRACKETS[i-1].bracket) * TAX_BRACKETS[i].rate;
+    const maxTaxBracketAmounttoPay = (income - TAX_BRACKETS[i-1].bracket) * rateToActualDecimal(TAX_BRACKETS[i].rate);
     taxToPay += maxTaxBracketAmounttoPay;
     break;      
   }
@@ -26,5 +26,9 @@ function calculateTax(income) {
 function calculateMaxBracketAmount(taxBracketIndex) {
   if (taxBracketIndex == 0) return 0; // Account for 0 income;
   const totalIncomeForBracket = TAX_BRACKETS[taxBracketIndex].bracket - TAX_BRACKETS[taxBracketIndex - 1].bracket;
-  return totalIncomeForBracket * TAX_BRACKETS[taxBracketIndex].rate;
+  return totalIncomeForBracket * rateToActualDecimal(TAX_BRACKETS[taxBracketIndex].rate);
+}
+
+function rateToActualDecimal(rate) {
+  return rate/100;
 }
